f361a1e7e34841474fe6d0ee512b8d74575124b8
Anastasia Simolova <simolovaai@gmail.com>
2023-01-19 00:01:23 +0300

    fix

--- src/test/java/usertest/UpdateUserDataWithoutAuthTest.java
+++ src/test/java/usertest/UpdateUserDataWithoutAuthTest.java
@@ -1,10 +1,9 @@
-package userTest;
+package usertest;
 
 import api.User;
 import api.UserClient;
 import io.qameta.allure.junit4.DisplayName;
 import io.restassured.response.ValidatableResponse;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,25 +16,26 @@ public class UpdateUserDataWithoutAuthTest {
     String newEmail;
 
     @Before
-    public void createNewUser() {
+    public void setUp() {
         userClient = new UserClient();
         user = User.getRandomUser();
         userClient.create(user);
     }
 
     @Test
-    @DisplayName("Изменение пользователя без авторизации. Ошибка!")
+    @DisplayName("Изменение пользователя. Пользователь с такой почтой уже существует!")
     public void changeUserEmailWithoutAuthTest() {
-        token = RandomStringUtils.randomAlphabetic(171);
-
-        newEmail = RandomStringUtils.randomAlphabetic(7) + "@mail.ru";
+        User userClientTwo = User.getRandomUser();
+        userClient.create(userClientTwo);
+        newEmail = userClientTwo.email;
+        token = userClient.login(user).extract().path("accessToken");
         response = userClient.updateUserInfo(token, "{\"email\": \"" + newEmail + "\"}");
 
-        response.assertThat().statusCode(401);
+        response.assertThat().statusCode(403);
         response.assertThat().extract().path("success").equals(false);
-        response.assertThat().extract().path("message").equals("You should be authorised");
-    }
+        response.assertThat().extract().path("message").equals("User with such email already exists");
 
+    }
     @After
     public void tearDown() {
         userClient.delete(token);
